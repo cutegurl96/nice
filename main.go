a800e05f67be5eedd4c68e851064fd7c2c0f958f
cutegurl96 <maddaywen@gmail.com>
2021-06-29 16:19:08 +0700

    Hello World

--- main.go
+++ main.go
@@ -1,135 +1,14 @@
-package gogit
+package main
 
 import (
-	"database/sql"
 	"fmt"
-	"html/template"
-	"log"
 	"net/http"
-	"time"
 )
 
-type ContactDetails struct {
-	Email   string
-	Subject string
-	Message string
-}
-
-func Gogit() {
-	fmt.Println("hello world")
-
-	tmpl := template.Must(template.ParseFiles("form.html"))
-
+func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
-			return
-		}
-
-		details := ContactDetails{
-			Email:   r.FormValue("email"),
-			Subject: r.FormValue("subject"),
-			Message: r.FormValue("message"),
-		}
-
-		// do something with details
-		_ = details
-
-		tmpl.Execute(w, struct{ Success bool }{true})
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	http.ListenAndServe(":8888", nil)
-}
-
-func main() {
-	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/root?parseTime=true")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	{ // Create a new table
-		query := `
-            CREATE TABLE users (
-                id INT AUTO_INCREMENT,
-                username TEXT NOT NULL,
-                password TEXT NOT NULL,
-                created_at DATETIME,
-                PRIMARY KEY (id)
-            );`
-
-		if _, err := db.Exec(query); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	{ // Insert a new user
-		username := "johndoe"
-		password := "secret"
-		createdAt := time.Now()
-
-		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		id, _ := result.LastInsertId()
-		fmt.Println(id)
-	}
-
-	{ // Query a single user
-		var (
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		)
-
-		query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
-		if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println(id, username, password, createdAt)
-	}
-
-	{ // Query all users
-		type user struct {
-			id        int
-			username  string
-			password  string
-			createdAt time.Time
-		}
-
-		rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer rows.Close()
-
-		var users []user
-		for rows.Next() {
-			var u user
-
-			err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
-			if err != nil {
-				log.Fatal(err)
-			}
-			users = append(users, u)
-		}
-		if err := rows.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Printf("%#v", users)
-	}
-
-	{
-		_, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	http.ListenAndServe(":80", nil)
 }
