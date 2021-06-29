468615c85376c2b1a6ba3654fe1725b4fcc40727
cutegurl96 <maddaywen@gmail.com>
2021-06-29 15:03:40 +0700

    nambah

--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,15 @@
 package gogit
 
+import "fmt"
+
 func SayHello() string {
 	return "Hellowwww"
+}
+
+func init() {
+	fmt.Println("imp-init() come here.")
+}
+
+func Print() {
+	fmt.Println("Hello!")
 }
